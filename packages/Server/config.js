27850b763d72da37e0fa183f32ad98c70b411263
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-05-21 17:06:10 -0500

    Creacion del clone de Amazono Clone

---
+++ packages/Server/config.js
@@ -0,0 +1,5 @@
+module.exports =   {
+    database:'mongodb://stockerssld:127ad127@ds237196.mlab.com:37196/amazonclone',
+    port:'3000',
+    secret: 'JoseAntonioPadreGarcia'
+}
---
+++ packages/Server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect(config.database,(err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Connected to the database  ')
+    }
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('dev'))
+const config = require('./config')
+
+app.get('/',(req, res, next)=>{
+    res.json({
+        user: 'JAPG'
+    })
+})
+app.listen(config.port,(err)=>{
+    console.log(`Magin happens on port ${config.port}` )
+})
