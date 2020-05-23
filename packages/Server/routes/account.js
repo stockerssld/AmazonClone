091ed752ac4e89c99fb3ba5463fd99c1abd1cb76
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-05-23 10:13:21 -0500

    change address and profile sever

--- packages/Server/routes/account.js
+++ packages/Server/routes/account.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user')
 const config = require('./../config')
+const checkJWT = require('./../middlewares/check.jwt')
 router.post('/signup',(req, res, next)=>{
     // console.log(req.body);
     const {name, email,password, isSeller}= req.body
@@ -71,5 +72,62 @@ router.post('/login', (req, res, next) => {
       } 
     });
   });
+
+router.route('/profile')
+.get(checkJWT,(req, res, next)=>{
+  User.findOne({_id: req.decoded.user._id}, (err, user)=>{
+    res.json({
+      success: true,
+      user: user, 
+      message: "Successful"
+    })
+  })
+})
+.post(checkJWT, (req, res, next)=>{
+  User.findOne({_id: req.decoded.user._id}, (err, user)=>{
+    const {name, email, password, isSeller} = req.body
+    if(err) return next(err)
+    if(name) user.name = name
+    if(email) user.email = email
+    if(password) user.password = password
+
+    user.isSeller = isSeller
+
+    user.save()
+    res.json({
+      success: true,
+      message: 'Successfully edited your profile.'
+    })
+  })
+})
+router.route('/address')
+  .get(checkJWT,(req, res, next)=>{
+    User.findOne({_id: req.decoded.user._id}, (err, user)=>{
+      res.json({
+        success: true,
+        address: user.address, 
+        message: "Successful"
+      })
+    })
+  })
+  .post(checkJWT, (req, res, next)=>{
+    User.findOne({_id: req.decoded.user._id}, (err, user)=>{
+      // const {addr1, email, password, isSeller} = req.body
+      console.log(req.body)
+     if(err) return next(err)
+     if(req.body.addr1) user.address.addr1 = req.body.addr1
+     if(req.body.addr2) user.address.addr2 = req.body.addr2
+     if(req.body.city) user.address.city = req.body.city
+     if(req.body.country) user.address.country = req.body.country
+     if(req.body.postalCode) user.address.postalCode = req.body.postalCode
+
+     
+      user.save()
+      res.json({
+        success: true,
+        message: 'Successfully edited your address.'
+      })
+    })
+  })
 
 module.exports = router
