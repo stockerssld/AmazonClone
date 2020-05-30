3649dfc0c4ad6843b1de8314ae2884dbe5c0131c
stockerssld <35477074+stockerssld@users.noreply.github.com>
2020-05-29 20:12:05 -0500

    Create metod save prodcuts aws s3 image Server

--- packages/Server/routes/seller.js
+++ packages/Server/routes/seller.js
@@ -4,8 +4,47 @@ const Product = require('../models/product')
 
 const aws = require('aws-sdk')
 const multer= require('multer')
-const multer2= require('multer-s3')
+const multer3= require('multer-s3')
 const s3= new aws.S3({accessKeyId:"AKIASPA4H475DDWMVHPS", secretAccessKey:"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"})
+
+const checkJWT = require('./../middlewares/check.jwt')
+const upload = multer({
+    storage: multer3({
+        s3: s3,
+        bucket: 'amazonowebappapplication',
+        metadata: function(req, file, cb){
+            cb(null, {fieldName: file.fieldname})
+        },
+        key: function(req, file, cb){
+            cb(null, Date.now().toString())
+        }
+    })
+})
+
+router.route('/products')
+    .get()
+    .post([checkJWT, upload.single('product_picture')],(req, res, next)=>{
+        
+        const {categoryId, title, price, description} = req.body
+        const {location} = req.file
+
+        const owner = req.decoded.user._id
+
+        let product = new Product()
+        product.owner = owner
+        product.category = categoryId
+        product.title = title
+        product.price = price
+        product.descripcion = description
+        product.image = location
+        product.save()
+        
+        res.json({
+            success: true,
+            message: 'Succesfully Added the product.'
+        })
+
+    })
 
 module.exports = router;
 
